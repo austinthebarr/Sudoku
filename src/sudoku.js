ede41fe12ef5f0a7f5f55f82f8b1b6d7ccdf7388
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-07-31 18:09:46 -0700

    for is now looping multiple times, test can now pass and fail

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,15 +1,21 @@
 export function Sudoku(board) {
   this.board = board;
 }
+var desiredList = [1,2,3,4,5,6,7,8,9];
 
-Sudoku.prototype.checkRow = function(arr) {
-  var sumRow = 0;
-  for (var i = 0; i < arr.lengthl i++) {
-    sumRow +=arr[i];
+Sudoku.prototype.checkRow = function() {
+  console.log(this.board)
+  var checkArra = this.board.slice();
+  console.log(checkArra);
+  for(var i = 0; i < checkArra.length; i++){
+    var sortArra = checkArra[i].sort();
+    console.log(sortArra);
+    for(var j = 0; j < sortArra.length; j++)
+    {
+      if(sortArra[j] != desiredList[j]){
+        return false;
+      }
+    }
   }
-  return sumRow;
-  if (sumRow === 45){
-    return "the row is good"
-  }
+  return true;
 };
-arr[row1[0], row2[0], ]
